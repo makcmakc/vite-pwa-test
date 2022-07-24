727655c072e8dddefdf4d569c41549e5ddcbb63e
Макс <makcmakc321@mail.ru>
2022-07-24 12:25:00 +0300

    init

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { VitePWA } from "vite-plugin-pwa"
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      mode: "development",
+      base: "/",
+      srcDir: "src",
+      filename: "sw.ts",
+      includeAssets: ["/favicon.png"],
+      strategies: "injectManifest",
+      manifest: {
+        name: "Test Project",
+        short_name: "Test",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+      },
+    }),
+  ],
+})
